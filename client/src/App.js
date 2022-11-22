dbae1074ce4020210f5c4c56a7b0a46059294fd7
fabian C <fabiancarrion1@gmail.com>
2022-11-22 17:17:19 -0500

    add color variables and logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import {BrowserRouter, Route, Switch } from "react-router-dom" 
 import Home from './components/home/Home';
+import LandingPage from './components/landingPage/LandingPage';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
       <Switch>
-        <Route exact path="/" component={Home}/>
+        <Route exact path="/home" component={Home}/>
+        <Route exact path="/" component={LandingPage}/>
       </Switch>
       </div>
     </BrowserRouter>
