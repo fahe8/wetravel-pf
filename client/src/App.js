d7824257e5f5b56c55f953250a2133e4060214d7
andradaalucas <andradalucaswork@gmail.com>
2022-11-23 09:16:27 -0300

    Creacion de componente

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import {BrowserRouter, Route, Switch } from "react-router-dom" 
 import Home from './components/home/Home';
+import Login from './components/log-in/Log-in';
+import Signup from './components/Sign-up/Sign-up';
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <div className="App">
       <Switch>
         <Route exact path="/" component={Home}/>
+        <Route exact path="/Login" component={Login}/>
+        <Route exact path="/Signup" component={Signup}/>
       </Switch>
       </div>
     </BrowserRouter>
--- client/src/components/log-in/Log-in.jsx
+++ client/src/components/log-in/Log-in.jsx
@@ -1,8 +1,57 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+
 
 function Login() {
   return (
-    <div>Log-in</div>
+    <div>
+      <h5 className="text-3xl ">Iniciar Sesion</h5>
+        <form>
+          <label  className=''>
+            Usuario
+          </label> 
+          <input  type="text" className=''>
+          </input>
+          <label className=''>
+            Contraseña
+          </label>
+          <input type="password" className=''/>
+          <button>
+            ingresar 
+          </button>
+        </form>
+        <div className='flex justify-center'>
+          <Link to="/Signup">
+            <h5>Crear usuario</h5>
+          </Link>
+        </div>
+        <p>o</p>
+        <div className='center block'>
+          <button className=''>
+            <img className='inline' src="https://cdn-icons-png.flaticon.com/128/174/174848.png" width="30px" height="30px" alt="not found" />
+            <span className='' >Registrarse con Facebook
+            </span>
+          </button>
+          <button>
+            <img className='inline' src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="30px" height="30px" alt="not found" />
+            <span>Registrarse con Linkedin
+            </span>
+          </button>
+          <button>
+            <img className='inline' src="https://cdn-icons-png.flaticon.com/128/281/281764.png" width="30px" height="30px" alt="not found" />
+            <span>Registrarse con Google
+            </span>
+          </button>
+          <button >
+            <img className='inline' src="https://cdn-icons-png.flaticon.com/512/174/174876.png" width="30px" height="30px" alt="not found" />
+            <span>Registrarse con twitter
+            </span>
+          </button>
+        </div>
+    </div>
+  
   )
 }
 
