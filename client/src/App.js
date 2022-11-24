55f11241ea60a1bf5858da6e6770cd03ad126c4b
andradaalucas <andradalucaswork@gmail.com>
2022-11-24 09:21:35 -0300

    nuevos cambios

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/home/Home";
 import LandingPage from "./components/landingPage/LandingPage";
 import NavBar from "./components/navBar/NavBar";
+import Login from "./components/log-in/Log-in";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Switch>
           <Route exact path="/home" component={Home} />
           <Route exact path="/" component={LandingPage} />
+          <Route exact path="/login" component={Login} />
         </Switch>
       </div>
     </BrowserRouter>
