ecad928fbd5b1de25e7b2d4e6ebad6cbf149114b
andradaalucas <andradalucaswork@gmail.com>
2022-11-24 01:37:01 -0300

    cambios en app

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {BrowserRouter, Route, Switch } from "react-router-dom" 
 import Home from './components/home/Home';
 import LandingPage from './components/landingPage/LandingPage';
+import Login from './components/log-in/Log-in';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Switch>
         <Route exact path="/home" component={Home}/>
         <Route exact path="/" component={LandingPage}/>
+        <Route exact path="/Login" component={Login}/>
       </Switch>
       </div>
     </BrowserRouter>
