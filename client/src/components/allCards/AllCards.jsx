01ddacd1516a8b162d8fc211908e4d72e745718b
andradaalucas <andradalucaswork@gmail.com>
2022-11-24 09:19:17 -0300

    nuevos cambios

--- client/src/components/allCards/AllCards.jsx
+++ client/src/components/allCards/AllCards.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from "react-redux"
-import Card from '../card/Card'
+import Card from '../card/card'
 import { getHotels } from '../../redux/action'
 
 
