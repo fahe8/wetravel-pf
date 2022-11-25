7bb4e4f06ee72615605519b5dfc1bc417349ac1f
Lucas Montecino <lucasmontecino95@gmail.com>
2022-11-25 12:03:11 -0300

    some changes

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 import { getDetail } from '../../redux/action/index'
 import NavBar from '../navBar/NavBar'
 
@@ -11,7 +12,10 @@ const Detail = (props) => {
     const selectedHotel = useSelector((state) => state.detail)
 
     useEffect(() => {
-        dispatch(getDetail(id));
+        dispatch(getDetail(id))
+        return()=>{
+            dispatch(getDetail(""));
+        };
     }, [dispatch, id])
 
     console.log(selectedHotel)
@@ -19,35 +23,45 @@ const Detail = (props) => {
     return (
         <div>
             <NavBar />
+            
             <div>
-
+            <Link to="/home">Return</Link>
                 <div>
-                    <div>
-                        <img src={selectedHotel.photos} alt="hotel" />
-                        {/* Carrusel de imágenes */}
-                    </div>
-                    <div>
-                        <h2>{selectedHotel.name}</h2>
-                        <h3>{selectedHotel.stars}</h3>
-                        <h3>{selectedHotel.location}, {selectedHotel.city}</h3>|
-                        <h3>{selectedHotel.size}</h3>
-                        <h3>{selectedHotel.price}</h3>
-                        <br />
+                    {
+                        selectedHotel ?
                         <div>
-                            <h2>Description: </h2>
-                            <hr />
-                            <p>{selectedHotel.description}</p>
+                            <div>
+                                <img src={selectedHotel.photos} alt="hotel" />
+                                {/* Carrusel de imágenes */}
+                            </div>
+                            <div>
+                                <h2>{selectedHotel.name}</h2>
+                                <h3>{selectedHotel.stars}</h3>
+                                <h3>{selectedHotel.location}, {selectedHotel.city}</h3>|
+                                <h3>{selectedHotel.size}</h3>
+                                <h3>{selectedHotel.price}</h3>
+                                <br />
+                            </div>
+                        
+                            <div>
+                                <h2>Description: </h2>
+                                <hr />
+                                <p>{selectedHotel.description}</p>
+                            </div>
+                            <br />
+                            <div>
+                                <h2>What does the place affter</h2>
+                                <hr />
+                            </div>
+                            <div>
+                                <p> {selectedHotel.comments}</p>
+                            </div>
+                        
                         </div>
-                        <br />
-                        <div>
-                            <h2>What does the place affter</h2>
-                            <hr />
-                        </div>
-                        <div>
-                            <p> {selectedHotel.comments}</p>
-                        </div>
-
-                    </div>
+                    : <p>Loading...</p>
+                    };
+                    
+                    
                 </div>
 
             </div>
