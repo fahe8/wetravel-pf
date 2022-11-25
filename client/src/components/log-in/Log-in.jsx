020f1cf493f9a294406802034681e5ed22f84fbe
andradaalucas <andradalucaswork@gmail.com>
2022-11-25 11:42:08 -0300

    nuevos cambios en login

--- client/src/components/log-in/Log-in.jsx
+++ client/src/components/log-in/Log-in.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import {useAuth0} from "@auth0/auth0-react"
+import Profile from '../profile/profile';
 
 
 function Login() {
   
-  const {loginWithRedirect} = useAuth0()
-  const {logout} = useAuth0()
+  const {loginWithRedirect, logout, user} = useAuth0()
 
   return (<>
-
-        <button onClick={() => loginWithRedirect() }>Log-in</button>
-        <button onClick={() => logout({ returnTo: window.location.origin })}>Log-out</button>
-  </>
+    {
+      user ?
+        (<img src={user.picture} alt={user.name}/>)
+        (<button className='w-[100px] h-[50px] border border-black mx-auto bg-[#00B4FF]' onClick={() => logout({ returnTo: window.location.origin })}>Log-out</button>)
+        
+        :(<button className="w-[100px] h-[50px] border border-black mx-auto bg-[#00B4FF] " onClick={() => loginWithRedirect() }>Log-in</button>)
+  
+  }</>
   )
 }
 
