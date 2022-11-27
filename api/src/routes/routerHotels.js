eb3450f70841a7b7324966dbd85a01f991af8255
carobarragan <104798095+carobarragan@users.noreply.github.com>
2022-11-27 12:31:27 -0300

    problemas resueltos

--- api/src/routes/routerHotels.js
+++ api/src/routes/routerHotels.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-const { Hotel, User } = require("../db");
-=======
 const { Hotel, User, Service } = require("../db");
->>>>>>> 7da0cd46d63fa909279c7e2621c839bcda6e1e57
 const { Router } = require("express");
 const routerHotels = Router();
 const apiData = require("../../hotels.json");
@@ -18,8 +14,6 @@ const getHotels = async () => {
           attributes: [],
         },
       },
-<<<<<<< HEAD
-=======
       {
         model: Service,
         attributes: ["name"],
@@ -27,16 +21,10 @@ const getHotels = async () => {
           attributes: [],
         },
       },
->>>>>>> 7da0cd46d63fa909279c7e2621c839bcda6e1e57
     ],
   });
 
   if (!dataDb.length) {
-<<<<<<< HEAD
-    const apiInfo = apiData.map((el) => {
-      return {
-        id: el.id,
-=======
     const servicesRepeat = apiData
       .map((data) => data.services)
       .join()
@@ -51,30 +39,14 @@ const getHotels = async () => {
     );
     apiData.map(async (el) => {
       const addHotelDb = await Hotel.create({
->>>>>>> 7da0cd46d63fa909279c7e2621c839bcda6e1e57
         name: el.name,
         description: el.description,
         stars: el.stars,
         price: el.price,
-<<<<<<< HEAD
-        services: el.services.map((ele) => ele),
-=======
->>>>>>> 7da0cd46d63fa909279c7e2621c839bcda6e1e57
         photos: el.photos.map((ele) => ele),
         continent: el.continent,
         location: el.location,
         city: el.city,
-<<<<<<< HEAD
-
-        review: el.review,
-        comments: el.comments,
-        room: el.room,
-      };
-    });
-    const hotels = await Hotel.bulkCreate(apiInfo);
-    // console.log(hotels);
-    return hotels;
-=======
         review: el.review,
         comments: el.comments,
         room: el.room,
@@ -87,7 +59,6 @@ const getHotels = async () => {
       });
       addHotelDb.addService(relacion);
     });
->>>>>>> 7da0cd46d63fa909279c7e2621c839bcda6e1e57
   }
   if (dataDb.length) {
     return dataDb;
@@ -95,12 +66,6 @@ const getHotels = async () => {
 };
 
 routerHotels.get("/", async (req, res) => {
-<<<<<<< HEAD
-  const { name, continent, stars, location, city, price } = req.query;
-  try {
-    const hotels = await getHotels();
-
-=======
   const {
     name,
     continent,
@@ -115,7 +80,6 @@ routerHotels.get("/", async (req, res) => {
   try {
     const hotels = await getHotels();
 
->>>>>>> 7da0cd46d63fa909279c7e2621c839bcda6e1e57
     if (name && hotels.length) {
       const hotelName = hotels.find((el) =>
         el.name.toLowerCase().includes(name.toLowerCase())
@@ -124,97 +88,61 @@ routerHotels.get("/", async (req, res) => {
         ? res.status(200).send(hotelName)
         : res.status(404).send("No Hotel with that name");
     }
-<<<<<<< HEAD
-    if (stars) {
-      try {
-        let filterStars = hotels.filter((el) => el.stars === stars);
-        console.log("FILTRO:", filterStars.length);
-        if (!filterStars.length) {
-          return res.status(400).send("No match  with that star");
-        }
-        return res.send(filterStars);
-      } catch (error) {
-        res.status(400).send(error.message);
-      }
-    }
-    if (price) {
-      try {
-        let filterPrice = hotels.filter((el) => el.price === price);
-        // console.log('FILTRO price:', filterPrice.length);
-        if (!filterPrice.length) {
-          return res.status(400).send("No match with that price");
-        }
-        return res.json(filterPrice);
-      } catch (error) {
-        res.status(400).send(error.message);
-      }
-    }
-=======
 
     if (stars || priceMin || priceMax || servicies) {
+      let filtrado = [];
+      if (priceMax && priceMin) {
+        let pri = hotels.filter((el) => {
+          return (
+            el.price.split("$")[1].split(".").join("") <= Number(priceMax) &&
+            el.price.split("$")[1].split(".").join("") >= Number(priceMin)
+          );
+        });
+        filtrado = pri;
+      }
 
-
-    let filtrado = []
-    if (priceMax && priceMin) {
-      let pri = hotels.filter((el) => {
-        return (
-          el.price.split("$")[1].split(".").join("") <= Number(priceMax) &&
-          el.price.split("$")[1].split(".").join("") >= Number(priceMin)
-        );
-        
-        
-      });
-      filtrado = pri
-    } 
-
-    if(stars) {
-        if(filtrado.length > 0) {
-            let filterStars = filtrado.filter((el) => el.stars === stars);
-            filtrado = filterStars;
+      if (stars) {
+        if (filtrado.length > 0) {
+          let filterStars = filtrado.filter((el) => el.stars === stars);
+          filtrado = filterStars;
         } else {
-
-            let filterStars = hotels.filter((el) => el.stars === stars);
-             filtrado = filterStars;
+          let filterStars = hotels.filter((el) => el.stars === stars);
+          filtrado = filterStars;
         }
+      }
+
+      return res.send(filtrado);
+
+      // if (servicies.includes(",")) {
+
+      //   const serviciesSearch = servicies.split(",");
+
+      //   await Hotel.findAll({
+      //     where: {price: {[Op.between]: [priceMin, priceMax]}, stars: stars },
+      //     include: {
+      //       model: Service,
+      //       attributes: ["name"],
+      //       where: {name: serviciesSearch},
+      //       through: {
+      //         attributes: [],
+      //       },
+      //     },
+      //   });
+      // }
+
+      // await Hotel.findAll({
+      //     where: {price: {[Op.between]: [priceMin, priceMax]}, stars: stars },
+      //     include: {
+      //       model: Service,
+      //       attributes: ["name"],
+      //       where: {name: servicies},
+      //       through: {
+      //         attributes: [],
+      //       },
+      //     },
+      //   });
     }
 
-    return res.send(filtrado)
-    
-        // if (servicies.includes(",")) {
-
-        //   const serviciesSearch = servicies.split(",");
-
-        //   await Hotel.findAll({
-        //     where: {price: {[Op.between]: [priceMin, priceMax]}, stars: stars },
-        //     include: {
-        //       model: Service,
-        //       attributes: ["name"],
-        //       where: {name: serviciesSearch},
-        //       through: {
-        //         attributes: [],
-        //       },
-        //     },
-        //   });
-        // }
-
-        // await Hotel.findAll({
-        //     where: {price: {[Op.between]: [priceMin, priceMax]}, stars: stars },
-        //     include: {
-        //       model: Service,
-        //       attributes: ["name"],
-        //       where: {name: servicies},
-        //       through: {
-        //         attributes: [],
-        //       },
-        //     },
-        //   });
-      
-
-
-      
-    }
-
->>>>>>> 7da0cd46d63fa909279c7e2621c839bcda6e1e57
     if (continent) {
       try {
         let filtercontinent = hotels.filter((el) =>
@@ -305,10 +233,6 @@ routerHotels.post("/", async (req, res) => {
       description,
       stars,
       price,
-<<<<<<< HEAD
-      services,
-=======
->>>>>>> 7da0cd46d63fa909279c7e2621c839bcda6e1e57
       photos,
       continent,
       location,
@@ -321,17 +245,12 @@ routerHotels.post("/", async (req, res) => {
     let userDb = await User.findAll({
       where: { name: user },
     });
-<<<<<<< HEAD
-
-    newHotel.addUser(userDb);
-=======
     let servicesDb = await Service.findAll({
       where: { name: services },
     });
 
     newHotel.addUser(userDb);
     newHotel.addService(servicesDb);
->>>>>>> 7da0cd46d63fa909279c7e2621c839bcda6e1e57
     res.status(200).send(newHotel);
   } catch (error) {
     res.status(400).send(error.message);
