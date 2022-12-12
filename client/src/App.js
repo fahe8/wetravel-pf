af6ad0c20b434fff474dc2cbae53201cc27ea4c2
Lucas Montecino <lucasmontecino95@gmail.com>
2022-12-12 19:34:34 -0300

    finish paginate

--- client/src/App.js
+++ client/src/App.js
@@ -31,6 +31,7 @@ import { AdminReviews } from "./components/Dashboard/AdminReviews";
 import { AccessDenied } from "./components/Dashboard/AccessDenied";
 import { AdminImages } from "./components/Dashboard/AdminImages";
 import Confirmacion from "./components/confirmacion/Confirmacion"
+import Paginate from "./components/Paginate/Paginate"
 
 
 function App() {
@@ -77,6 +78,7 @@ function App() {
           <Route exact path="/admin-images" component={AdminImages} />
           <Route exact path="/access-denied" component={AccessDenied} />
           <Route exact path ="/confirmacion" component = {Confirmacion} />
+          <Route exact path="/test" component = {Paginate}></Route>
 
           <Route path="#" element={<Home />} />
         </Switch>
