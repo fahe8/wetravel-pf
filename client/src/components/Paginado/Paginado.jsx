a976b85a8a6e5dc529d88d59f5bc365ca1dcf228
juan-rv <juanrodriguez9502@gmail.com>
2022-12-14 14:07:56 -0500

    estilos

--- client/src/components/Paginado/Paginado.jsx
+++ client/src/components/Paginado/Paginado.jsx
@@ -12,9 +12,13 @@ export default function Paginado({hotels,currentPage,setCurrentPage,search}){
         setCurrentPage(currentPage-12)
     }
     return(
-        <div>
-        <button  onClick={prevPage}>Prev Page</button>
-        <button  onClick={nextPage}>Next Page</button>
+        <div className="flex justify-center items-center">
+            <div className="bg-[color:var(--second-bg-color)] shadow-[inset_0_-2px_4px_rgba(0,0,0,0.6)]  hover:shadow-[inset_0_4px_4px_rgba(0,0,0,0.25)] rounded-[10px] p-2 mx-2 ">
+                <button onClick={prevPage}>Prev Page</button>
+            </div>
+            <div className="bg-[color:var(--second-bg-color)] shadow-[inset_0_-2px_4px_rgba(0,0,0,0.6)]  hover:shadow-[inset_0_4px_4px_rgba(0,0,0,0.25)] rounded-[10px] p-2 mx-2">
+                <button onClick={nextPage}>Next Page</button>
+            </div>
         </div>
     )
 }
