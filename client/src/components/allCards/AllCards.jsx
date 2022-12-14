de1b743642d1b0cf7b3bd132a06d4e3957822c26
[santiagogomezz] <[santiagog2002@gmail.com]>
2022-12-14 13:16:51 -0300

    paginado y search

--- client/src/components/allCards/AllCards.jsx
+++ client/src/components/allCards/AllCards.jsx
@@ -7,54 +7,93 @@ import { Footer } from "../footer/Footer";
 import { useLocalStorage } from "../../localStorage/useLocalStorage";
 import InfiniteScroll from 'react-infinite-scroll-component';
 import LoadHome from "../Loading/loadingHome/LoadHome";
+import Paginado from "../Paginado/Paginado"
 
 const AllCards = () => {
   let estadoHoteles = useSelector((state) => state.hotels); 
   const hasMore = useSelector((state) => state.hasMore)
   const dispatch = useDispatch();
   const [page, setPage] = useState(1);
+  const [search,setSearch] = useState("")
+  const [order,setOrder] = useState("")
+  const [currentPage, setCurrentPage] = useState(0)
+  const hotels = useSelector((state)=>state.hotels)
+  
+  function filteredHotels(){
+    if(search.length===0){
 
-  const fetchMoreData = () => {
-        setPage(prevState => prevState + 1)
-        setTimeout(() => {
-          if(hasMore){
-            dispatch(getHotels(page))
-          }
-        }, 1000);
-  };
+    let filter = hotels.slice(currentPage,currentPage + 12)
+   
+    return filter
+}
+const filtered = hotels.filter(el=> el.name.toLowerCase().includes(search.toLowerCase()))
+return filtered.slice(currentPage,currentPage + 12)
+}
+
+  // const fetchMoreData = () => {
+  //       setPage(prevState => prevState + 1)
+  //       setTimeout(() => {
+  //         if(hasMore){
+  //           dispatch(getHotels(page))
+  //         }
+  //       }, 1000);
+  // };
 
   const [favorites, setFavorites] = useLocalStorage("fav", []);
 
-  useEffect(() => {
-    dispatch(getHotels(1))
-    setPage(prevState=>prevState+1)
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(getHotels(1))
+  //   setPage(prevState=>prevState+1)
+  // }, [dispatch]);
+  const onSearch= ({target})=>{
+    setCurrentPage(0)
+    setSearch(target.value.toLowerCase())
+}
+   useEffect(() => {
+    dispatch(getHotels())
+  }, []);
 
   return (
     <div className=" h-full md:m-h-screen w-full">
       <div className="container mx-auto px-0 md:px-4 py-4">
-          <InfiniteScroll dataLength={estadoHoteles.length} next={fetchMoreData} hasMore={hasMore} loader={<LoadHome/>} className="flex flex-wrap justify-center gap-4">
-          {estadoHoteles.length > 0 ? (
-            estadoHoteles
-              .filter((h) => h.status !== false)
-              .map((ht) => (
-                <Card
-                  id={ht.id}
-                  key={ht.id}
-                  photos={ht.photos[0]}
-                  stars={ht.stars}
-                  location={ht.location}
-                  city={ht.city}
-                  price={ht.price}
-                  name={ht.name}
+      <div>
+            <input
+                type="text"
+                placeholder="Busca tu Hotel"
+                value={search}
+                onChange={onSearch}
+                >
+              </input>
+         </div>
+      <div>
+          <Paginado
+            currentPage={currentPage}
+            hotels ={hotels}
+            setCurrentPage={setCurrentPage}
+            search={search}
+           />
+       </div>
+      <div className="flex flex-wrap">
+      {
+        filteredHotels && filteredHotels().map((el)=>
+          <Card
+          className="ml-3"
+                  id={el.id}
+                  
+                  key={el.id}
+                  photos={el.photos[0]}
+                  stars={el.stars}
+                  location={el.location}
+                  city={el.city}
+                  price={el.price}
+                  name={el.name}
                   favorites={favorites}
                   setFavorites={setFavorites}
-                />
-              ))
-          ) : (
-            <LoadHome/>
-          )}
-          </InfiniteScroll>
+                />  
+        )
+      }
+    </div>
+                  
       </div>
       <div>
         <Footer />
